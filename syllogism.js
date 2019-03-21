0c8e0c94b08fd360e9bd6605acd9e8ee97de591d
elyseeelius <elyseeelius@outlook.com>
2019-03-20 23:56:16 -0400

    lastTouch

--- syllogism.js
+++ syllogism.js
@@ -2,21 +2,25 @@
 Socrates is a man.
 Therefore, socrates is mortal.*/
 
-let men = "mortal"
-if(socrate = men){
-  console.log('Socreate is mortal');
-}else{
-  console.log('Socrate is not mortal(men)')
-}
+let men = ["Socrate","Elysee","Pierre","Steeve"]; // A collection of item or Array.
 
+if(men.includes("Socrate")){ // Socrate is a man.
+   console.log('Socreate is mortal'); // Thus Socrate is mortal.
+}else{
+  console.log('Socrate is not mortal')
+}
 
 /*This cake is either vanilla or chocolate.
 This cake is not chocolate.
 Therefore, this cake is vanilla.*/
 
-let cake = 'vanilla';
-if(cake != 'chocolate'){
+let cake;
+cake = 'vanilla';
+
+if(cake !='chocolate'){
   console.log('Cake is vanilla');
 }else{
   console.log('Cake is chocolate')
 }
+
+// Thank you so much for your feedback.
