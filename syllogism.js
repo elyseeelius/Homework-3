5ad75e88ddac5d3a3174cdcefa9a0e3f8549d7d4
Elysee Elius <elyseeelius@outlook.com>
2019-03-21 01:35:13 -0300

    Update syllogism.js

--- syllogism.js
+++ syllogism.js
@@ -5,9 +5,9 @@ Therefore, socrates is mortal.*/
 let men = ["Socrate","Elysee","Pierre","Steeve"]; // A collection of item or Array.
 
 if(men.includes("Socrate")){ // Socrate is a man.
-   console.log('Socreate is mortal'); // Thus Socrate is mortal.
+   console.log('Socreate is mortal because he is a man.'); // Thus Socrate is mortal.
 }else{
-  console.log('Socrate is not mortal')
+  console.log('Socrate is not a man. He is not mortal');
 }
 
 /*This cake is either vanilla or chocolate.
